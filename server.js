f51a5a39150f830312db552edd53361d1bea90a3
Karthik <karthik.kaliyaperumal@innoart.in>
2022-04-11 15:38:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { exec } = require('child_process');
+
+const PORT = 3100
+
+const app = express()
+//const api = require('./routes/api')
+var result = exec('dir', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+
+app.use(bodyParser.json())
+// app.use('/api',api)
+
+app.get('/', function(req, res){
+    res.send(result);
+})
+
+app.listen(PORT, function(){
+    console.log('Server node running on localhost:'+ PORT)
+})
